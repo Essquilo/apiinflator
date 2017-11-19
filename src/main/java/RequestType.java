8af9d4400fcc8f8ccbcd4e0432e36a80346ea1e0
Ivan Prymak <esquilo1994@gmail.com>
2017-11-19 19:12:43 +0100

    Initial framework setup

---
+++ src/main/java/RequestType.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Ivan Prymak on 11/13/2017.
+ */
+public enum RequestType {
+    GET, PATCH, POST, PUT
+}
---
+++ src/main/java/Url.java
@@ -0,0 +1,13 @@
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by Ivan Prymak on 11/12/2017.
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Url {
+    String value();
+}
